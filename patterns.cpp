870f3c5b415e0b75b80396ae1ddab91cfddf4d37
Codngwthubaid <codngwthubaid@gmail.com>
2025-06-21 21:59:23 +0530

    solves - 7 and 8 pattern

--- patterns.cpp
+++ patterns.cpp
@@ -83,13 +83,58 @@ void pattern6(int num)
     }
 }
 
+void pattern7(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < num - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern8(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * num - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
 int main()
 {
-
     int num;
     cin >> num;
     cout << "No of lines : " << num << endl;
-    pattern6(num);
+    pattern8(num);
 
     return 0;
 }
