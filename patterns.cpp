653c3f6eb59f7fce4cce4fefbff22172e7e07b4f
Codngwthubaid <codngwthubaid@gmail.com>
2025-06-22 16:57:53 +0530

    completed all the 22 patterns

--- patterns.cpp
+++ patterns.cpp
@@ -213,13 +213,202 @@ void pattern14(int num)
     }
 }
 
+void pattern15(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (char ch = 'A'; ch < 'A' + (num - i + 1); ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch;
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern17(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        char ch = 'A';
+        int break_point = (2 * i + 1) / 2;
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= break_point)
+            {
+                ch++;
+            }
+            else
+            {
+                ch--;
+            }
+        }
+
+        for (int j = 0; j < num - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern18(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch;
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern19(int num)
+{
+    int init_space_1 = 0;
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num - i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < init_space_1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < num - i; j++)
+        {
+            cout << "*";
+        }
+        init_space_1 += 2;
+        cout << endl;
+    }
+    int init_space_2 = 2 * num - 2;
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < init_space_2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        init_space_2 -= 2;
+        cout << endl;
+    }
+}
+
+void pattern20(int num)
+{
+    int init_space = 2 * num - 2;
+    for (int i = 1; i <= 2 * num - 1; i++)
+    {
+        int stars = i;
+        if (i > num)
+        {
+            stars = 2 * num - i;
+        }
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= init_space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+
+        if (i < num)
+        {
+            init_space -= 2;
+        }
+        else
+        {
+            init_space += 2;
+        }
+    }
+}
+
+int pattern21(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num; j++)
+        {
+            if (i == 0 || j == 0 || i == num - 1 || j == num - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int pattern22(int num)
+{
+    for (int i = 0; i < 2 * num - 1; i++)
+    {
+        for (int j = 0; j < 2 * num - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * num - 2) - j;
+            int bottom = (2 * num - 2) - i;
+
+            int result = (num - min(min(top, bottom), min(left, right)));
+            cout << result;
+        }
+        cout << endl;
+    }
+}
+
 // combine pattern7 nad pattern8 function together for getting pattern 9
 int main()
 {
     int num;
     cin >> num;
     cout << "No of lines : " << num << endl;
-    pattern14(num);
+    pattern22(num);
 
     return 0;
 }
