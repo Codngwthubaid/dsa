e473b96dcd5898abea793ef0b90c7553f6da0ec9
Codngwthubaid <codngwthubaid@gmail.com>
2025-07-22 13:21:47 +0530

    solving basic problems of recursion

---
+++ recursion.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int c = 0;
+
+// void print()
+// {    if (c == 5)
+//         return;
+//     cout << c << endl;
+//     c++;
+//     print();
+// }
+
+// void printName()
+// {
+//     if (c == 5)
+//         return;
+//     cout << "Ubaid" << endl;
+//     c++;
+//     printName();
+// }
+
+// void print1toN(int n){
+//     if(c == n) return;
+//     cout << c << endl;
+//     c++;
+//     print1toN(n);
+// }
+
+int main()
+{
+    int n;
+    cout << "function starting" << endl;
+    cin >> n;
+    // print();
+    // printName();
+    // print1toN(n);
+    return 0;
+}
