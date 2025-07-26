50449514166ebbda9dac3639d60d85674c2abe3b
Codngwthubaid <codngwthubaid@gmail.com>
2025-07-26 13:57:17 +0530

    selection, bubble and insertion sort completes

---
+++ sorting.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int mini = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
+                mini = j;
+            }
+        }
+        swap(arr[mini], arr[i]);
+    }
+}
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "runs\n";
+    }
+}
+
+void inserttion_sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+            cout << "runs\n";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    // selection_sort(arr, n);
+    // bubble_sort(arr, n);
+    inserttion_sort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout
+            << arr[i] << " ";
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,3 @@
-
+void selection_sort(int arr[], int n)
+// {
+// }
